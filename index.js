b60606c3ec0deb149ca5083ac44eea21da622184
Akmal Izuddin <izuddin0407@gmail.com>
2022-05-31 12:08:33 +0800

    completed the site

--- index.js
+++ index.js
@@ -5,15 +5,31 @@ const fs = require( 'fs' );
 const port = 3000;
 
 const server = http.createServer( ( req, res ) => {
-  fs.readFile( 'index.html', ( err, data ) => {
-    if (err) {
-      res.writeHead( 404, { 'Content-Type': 'text/html' } );
+  if ( req.url === '/' ) {
+    fs.readFile( 'index.html', ( err, data ) => {
+      res.writeHead( 200, { 'Content-Type': 'text/html' } );
+      res.write( data );
       return res.end();
-    }
-    res.writeHead( 200, { 'Content-Type': 'text/html' } );
-    res.write( data );
-    return res.end();
-  })
+    } )
+  } else if ( req.url === '/about' ) {
+    fs.readFile( 'about.html', ( err, data ) => {
+      res.writeHead( 200, { 'Content-Type': 'text/html' } );
+      res.write( data );
+      return res.end();
+    } )
+  } else if ( req.url === '/contactMe' ) {
+    fs.readFile( 'contactMe.html', ( err, data ) => {
+      res.writeHead( 200, { 'Content-Type': 'text/html' } );
+      res.write( data );
+      return res.end();
+    } )
+  } else {
+    fs.readFile( '404.html', ( err, data ) => {
+      res.writeHead( 404, { 'Content-Type': 'text/html' } );
+      res.write( data );
+      return res.end();
+    } )
+  }
 });
 
 server.listen( port, () => {
